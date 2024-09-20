4b87f8c20ce6d644deb93f5c6825c8707c8973d8
max5oooooo <maxuel5oooooo@gmail.com>
2024-09-20 16:32:45 +0200

    start block1

---
+++ lesson_11/block1.js
@@ -0,0 +1,25 @@
+/*Crea un html con un paragrafo e stampa l'oggetto document
+console.log(document);*/
+
+
+/*Creare uno script che selezioni un elemento della pagina HTML tramite getElementById
+
+const prova1 = document.getElementById("prova1");*/
+
+
+
+/*Creare uno script che selezioni un elemento della pagina HTML tramite querySelector
+const prova1 = document.querySelector("#prova1");*/
+
+
+/*Crea un paragrafo e modificane il testo con innerText
+const testo = document.querySelector("#testo");
+
+testo.innerText = "il testo è stato modificato";*/
+
+/*Cambia lo stile del paragrafo modificando il background-color e il font-size tramite Javascript
+
+const testo = document.querySelector("#testo");
+
+testo.style.backgroundColor = "red";
+testo.style.fontSize = "24px";*/
