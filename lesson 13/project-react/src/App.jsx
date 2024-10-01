d717e90c5890d8358d463e43e61e53d677b9593e
max5oooooo <maxuel5oooooo@gmail.com>
2024-10-01 15:58:02 +0200

    fixed components

--- lesson 13/project-react/src/App.jsx
+++ lesson 13/project-react/src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css'
+import MyTitle from "./components/Mytitle";
 
 function App() {
   return (
-    <div>
-        <h1>Hello, world!</h1>
-      </div>
+  <>    
+    <MyTitle />
+  </>
   )
 }
 
---
+++ lesson 13/project-react/src/components/Mytitle.jsx
@@ -0,0 +1,9 @@
+const MyTitle = () => {
+    return (
+        <>
+            <h1>Hello World</h1>
+        </>
+    )
+}
+
+export default MyTitle;
